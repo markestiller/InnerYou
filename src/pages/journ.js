8aa610effe797dc934d9231ee1dc2e6abcab5a40
maleehaMirza <maleeha.mirza2004@gmail.com>
2023-08-26 22:21:45 -0400

    journal

---
+++ src/pages/journ.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import { ReactComponent as Background } from '../backgrounds/Desktop - 9.svg';
+
+const Login = () => {
+  return <div className="flex justify-end items-center">
+    <Background/>
+  </div>;
+};
+
+export default Login;
